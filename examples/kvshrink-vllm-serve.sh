beea3422b443317788829d17825e39675d6218bb
Bin Yang <bin.yang@intel.com>
2026-08-13 20:26:50 +0800

    perf: enable tcmalloc for vLLM serving

--- examples/kvshrink-vllm-serve.sh
+++ examples/kvshrink-vllm-serve.sh
@@ -5,6 +5,7 @@ source "$SCRIPT_DIR/../setvars.sh"
 : "${MODEL:?Set MODEL in setvars.sh or the environment}"
 
 export LD_PRELOAD="/usr/local/lib/libiomp5.so${LD_PRELOAD:+:$LD_PRELOAD}"
+export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4${LD_PRELOAD:+:$LD_PRELOAD}"
 
 vllm serve "$MODEL" \
     --kv-transfer-config '{"kv_connector":"KVShrinkConnector","kv_connector_module_path":"kvshrink.kvshrink_connector","kv_role":"kv_both"}' \
